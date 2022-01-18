8e8a5afc56adca1ce649e3d4033741d4638bb844
andressaquintanilha.email <quintanilhalg3@gmail.com>
2022-01-18 08:15:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//estrutura FOR
+for (let cont = 1; cont <= 10; cont+=1) {
+    console.log(cont)
+}
+
+//estrutura WHILE
+let cont = 10
+while (cont >= 1) {
+    console.log(cont)
+    cont -=1
+}
+
+//contador de números impares 
+for (let cont = 1; cont <= 100; cont+=2) {
+    console.log(cont)
+}
