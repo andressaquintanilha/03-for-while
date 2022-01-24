57e7cef43bb24644d311ed9a01edd72ea72d1406
andressaquintanilha.email <quintanilhalg3@gmail.com>
2022-01-24 09:23:24 -0300

    faltou um exercicio

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ while (cont >= 1) {
     cont -=1
 }
 
-//contador de números impares 
+//Criar um loop que conte todos os números ímpares de 1 até 100
 for (let cont = 1; cont <= 100; cont+=2) {
+    console.log(cont)
+}
+
+//criar um loop que conte todos os números pares de 0 a 100
+for (let cont = 0; cont <= 100; cont+=3) {
     console.log(cont)
 }
